6b6036d7b5decdbf0012a41d5a38a0da74e8a864
Rubalito <adrian.perez.garcia2018@gmail.com>
2019-06-14 19:59:24 +0200

    v_1.2 Optimizacion del codigo

--- js/motor.js
+++ js/motor.js
@@ -1,18 +1,15 @@
-const CP = 28027;
+const CP = 28014;
 const KEY = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZHJpYW4ucGVyZXouZ2FyY2lhMjAxOEBnbWFpbC5jb20iLCJqdGkiOiI0OWM1ZmJmZS03ODc3LTQyYzItOGY5Ny04NWNiY2VlMjZlNmYiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTU2MDMyMjE3MywidXNlcklkIjoiNDljNWZiZmUtNzg3Ny00MmMyLThmOTctODVjYmNlZTI2ZTZmIiwicm9sZSI6IiJ9.Hpv4mhwe0-2XJZrMmN9DtOJ-LEQClf_h5Gh02eCguis";
 const URL = `https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/${CP}/?api_key=${KEY}`;
 
-let cont = 0,
-    franjahoraria = "";
 var semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 let sun = 0,
-    // sunnub = 0,
     nub = 0,
     nubrain = 0,
-    // thundernub = 0,
-    // snow = 0,
-    // snownub = 0,
-    thundernubrain = 0;
+    thundernubrain = 0,
+    cont = 0,
+    franjahoraria = "";
+
 fetch(URL)
     .then(datos => datos.json())
     .then(tiempo => {
@@ -25,26 +22,23 @@ function carga(info) {
         .then(query => query.json())
         .then(aemet => {
             let fecha = new Date(),
-                posicionDia = fecha.getDay(),
+                // posicionDia = fecha.getDay(),
                 dia = fecha.getDate(),
                 hora = fecha.getHours(),
                 mes = "0" + (1 + fecha.getMonth()),
                 ano = fecha.getFullYear(),
                 fechaCompleta = ano + "-" + mes + "-" + dia;
-
             //Calculo de la franja horaria y cambio de fondo dependiendo de la hora
             cambioFondo(hora);
-
             for (let i = 0; i < aemet.length; i++) {
-                document.getElementById("titulo").innerHTML = `${aemet[i].provincia} ${aemet[i].id}`;
-
+                var probabilidad = aemet[0].prediccion.dia[0].probPrecipitacion[i].value;
+                document.getElementById("titulo").innerHTML = `${aemet[i].nombre} ${aemet[i].id}`;
                 // Calculamos el estado del cielo 
                 tiempoDia(aemet[i].prediccion.dia[1].estadoCielo[i].descripcion);
                 // Calculamos los grados
                 tiempoGrados(aemet[i].prediccion.dia, hora, fechaCompleta);
                 //Calculo del dia en el que estamos
-                calculoTiempXDia(aemet[i].prediccion.dia, hora);
-
+                calculoTiempXDia(aemet[i].prediccion.dia, hora, probabilidad);
             }
         });
 }
@@ -103,12 +97,11 @@ function calculoDiasSemana() {
     }
 }
 
-function calculoTiempXDia(x, h) {
-
+function calculoTiempXDia(x, r) {
     for (let z = 0; z < x.length; z++) {
-        let estadoCielo = Math.max(sun, nub, nubrain, thundernubrain);
+        // let estadoCielo = Math.max(sun, nub, nubrain, thundernubrain);
+        let estadollevar = "";
         for (let t = 0; t < x[z].estadoCielo.length; t++) {
-
             let estado = x[z].estadoCielo[t].descripcion;
             if (estado == "Despejado") {
                 sun++;
@@ -119,24 +112,35 @@ function calculoTiempXDia(x, h) {
             } else if (estado == "Tormenta") {
                 thundernubrain++;
             }
+            estadollevar = estado;
         }
-        calculoDiaActual(cont, h, estadoCielo);
+        calculoDiaActual(cont, x[z], estadollevar, r);
     }
 }
 
-function calculoDiaActual(a, h, e) {
+function calculoDiaActual(a, x, e, r) {
     if (a < 8) {
         actualday = semana[cont];
         cont++;
     } else {
         cont = 1;
-        actualday = semana[cont];
+        actualday = semana[cont - 1];
         cont++;
     }
-    $('#grafo').append('<div class="container"><p class="dia">' + actualday + '</p></div>');
-    // if (e == sun) {
-    //     $('.container').append('<div class="contimg"><img class="estadosky" src="png/022-sun.png"></div>');
-    // } else if (e == nub) {
-    //     $('.container').append('<div class="contimg"><img class="estadosky" src="png/023-sunny.png"</div>');
-    // }
+    $('#grafo').append(`<div class="container">
+        <h4 class="dia"> ${actualday} </h4>
+        <div class="cajita">
+            <p class="max">${x.temperatura.maxima} </p>
+            <p class="min">${x.temperatura.minima} </p>
+        </div>
+    </div>`);
+    // console.log(r);
+    console.log(e);
+    if (e == "Despejado") {
+        $('.container').append('<div class="contimg"><img class="estadosky" src="png/022-sun.png"></div>');
+    } else if (e == "Poco nuboso") {
+        $('.container').append('<div class="contimg"><img class="estadosky" src="png/023-sunny.png"</div>');
+    } else if (e == "nubes altas") {
+        $('.container').append('<div class="contimg"><img class="estadosky" src="png/023-sunny.png"</div>');
+    }
 }
